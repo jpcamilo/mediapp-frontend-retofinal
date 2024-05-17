06ad1b77431b950b4015fa98a6a8d89c84511f2c
Camilo <jpcamilo407@gmail.com>
2024-05-16 22:05:09 -0500

    Sincronizacion del proyecto inicial, correccion para ejecutar

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginService } from '../services/login.service';
+import { environment } from '../../environments/environment.development';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MaterialModule, ReactiveFormsModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  username: string;
+  password: string;
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ){}
+
+  login(){
+    this.loginService.login(this.username, this.password).subscribe(data => {
+      sessionStorage.setItem(environment.TOKEN_NAME, data.access_token);
+      this.router.navigate(['pages/dashboard'])
+    });
+  }
+
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    //HOST: 'http://104.131.170.146/mediapp-backend',
+    HOST: 'http://localhost:8080',
+    RETRY: 2,
+    TOKEN_NAME: 'access_token'
+};
